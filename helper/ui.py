4a08a203582415c5559e5c5630cfb28386d09eb9
dhopp1 <daniel.hopp1@gmail.com>
2025-04-22 22:12:00 +0200

    Update to use llads

--- helper/ui.py
+++ helper/ui.py
@@ -1,13 +1,40 @@
+from datetime import datetime, timedelta
+import extra_streamlit_components as stx
 import streamlit as st
 import hmac
 
 
+def get_cookie_manager():
+    return stx.CookieManager()
+
+
 def check_password():
     """Check if a user entered the password correctly"""
+    st.session_state["cookie_manager"] = get_cookie_manager()
+
+    if st.session_state["cookie_manager"].get(cookie="logged_in_statschat") == True:
+        st.session_state["password_correct"] = True
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+        try:
+            condition = hmac.compare_digest(
+                st.session_state["password"],
+                st.secrets["password"],
+            )
+        except:
+            condition = False
+
+        if condition:
+            # cookie
+            expires_at = datetime.now() + timedelta(days=30)
+            st.session_state["cookie_manager"].set(
+                cookie="logged_in_statschat",
+                val="true",
+                expires_at=expires_at,
+                key="logged_in_statschat",
+            )
+
             st.session_state["password_correct"] = True
             del st.session_state["password"]
         else:
